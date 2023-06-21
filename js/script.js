3f8d288b36dbbf977235f256dc13e65c1471728d
Alfonso Dionisio <134362662+Fondve@users.noreply.github.com>
2023-06-21 21:09:17 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function delete_Profile() {
+	let deletion = "Are you sure you want to delete your user profile?";
+		if (confirm(deletion) == true) {
+			deletion = alert("Sucessfully deleted! Redirecting to home page.");
+			location.href = 'guest_home.html';
+		}
+		else {
+			deletion = alert("Canceled!");
+		}
+	document.getElementById("deleteprofile").innerHTML = deletion;
+}
+
+function guest_Register() {
+         alert("You have to register first!");
+         location.href = 'register.html';
+        }
+
+function logout() {
+	let logout = "Are you sure you want to logout?";
+		if (confirm(logout) == true) {
+			logout = alert("Sucessfully logged out! Redirecting to home page.");
+			location.href = 'guest_home.html';
+		}
+		else {
+			logout = alert("Canceled!");
+		}
+	document.getElementById("logout").innerHTML = logout;
+}
+
+function edit_reservation() {
+	alert('Edited!');
+}
+
+function remove_reservation() {
+	alert('Removed!');
+}
