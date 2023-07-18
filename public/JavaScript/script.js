db0707363419de9bc495c030e3df9caed270a938
Jesca Ryanne Acosta <89693266+jescaryanneacosta@users.noreply.github.com>
2023-07-18 14:44:46 +0800

    Update script.js

--- public/JavaScript/script.js
+++ public/JavaScript/script.js
@@ -44,3 +44,28 @@ function redirectToPage() {
         window.location.href = "/labtechhome";
       }
     }
+function validateLogin() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if (username === "" || password === "") {
+	alert("Please fill in all the fields.");
+	} else {
+		redirectToPage();
+	}
+}
+
+function validateRegistration() {
+	var roles = document.getElementById("roles").value;
+	var firstname = document.getElementById("firstname").value;
+	var lastname = document.getElementById("lastname").value;
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("pw").value;
+
+	if (roles === "" || firstname === "" || lastname === "" || email === "" || password === "") {
+	alert("Please fill in all the fields.");
+	} else {
+	// Proceed with registration
+	redirectToPage();
+	}
+}
