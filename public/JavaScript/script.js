c0307792ba0f289e9e6753738abf30b3aea9877f
Alfonso Dionisio <134362662+Fondve@users.noreply.github.com>
2023-07-20 22:10:08 +0800

    Update script.js

--- public/JavaScript/script.js
+++ public/JavaScript/script.js
@@ -2,7 +2,7 @@ function delete_Profile() {
 	let deletion = "Are you sure you want to delete your user profile?";
 		if (confirm(deletion) == true) {
 			deletion = alert("Sucessfully deleted! Redirecting to home page.");
-			location.href = '../html/guest_home.html';
+			location.href = '/guesthome';
 		}
 		else {
 			deletion = alert("Canceled!");
@@ -12,14 +12,14 @@ function delete_Profile() {
 
 function guest_Register() {
          alert("You have to register first!");
-         location.href = '../html/register.html';
+         location.href = '/register';
         }
 
 function logout() {
 	let logout = "Are you sure you want to logout?";
 		if (confirm(logout) == true) {
 			logout = alert("Sucessfully logged out! Redirecting to home page.");
-			location.href = '../html/guest_home.html';
+			location.href = '/guesthome';
 		}
 		else {
 			logout = alert("Canceled!");
@@ -39,9 +39,9 @@ function redirectToPage() {
       var selectedRole = document.getElementById("roles").value;
 
       if (selectedRole === "student") {
-        window.location.href = "../html/student_home.html";
+        window.location.href = "/studenthome";
       } else if (selectedRole === "tech") {
-        window.location.href = "../html/labtech_home.html";
+        window.location.href = "/labtechhome";
       }
     }
 function validateLogin() {
