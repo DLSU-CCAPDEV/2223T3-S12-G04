12504dd70004838c91401997eadd7532f0ebb199
Alfonso Dionisio <134362662+Fondve@users.noreply.github.com>
2023-07-23 14:53:56 +0800

    Update and rename routes_draft.js to routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const controller = require('../controllers/controller_draft.js');
+const controller = require('../controllers/controller.js');
 
 const registerController = require('../controllers/registerController.js');
 
@@ -10,6 +10,10 @@ const profileController = require('../controllers/profileController.js');
 
 const homeController = require('../controllers/homeController.js');
 
+const reservationController = require('../controllers/reservationController.js');
+
+const loginController = require('../controllers/loginController.js');
+
 const app = express();
 
 
@@ -18,14 +22,20 @@ app.get('/favicon.ico', controller.getFavicon);
 app.get(`/`, controller.getRoot); // guest home page as default page
 app.get(`/guesthome`, controller.redirectRoot); // redirecting to guest home page
 
-app.get(`/login`, controller.redirectLogin); //calling function to LOGIN.hbs
+app.get(`/login`, loginController.getLogin); //calling function to LOGIN.hbs
+app.post(`/login`, loginController.postLogin);
 
 app.get(`/register`, registerController.getRegister); // calling function to REGISTER.hbs
 app.post(`/register`, registerController.postRegister); //calling function to REGISTER
 app.get(`/success`, successController.getSuccess);
+app.get(`/success`, successController.getHomeSuccess);
 app.get(`/home/:idNum`, homeController.getHome);
 
 app.get(`/profile/:idNum`, profileController.getProfile);
+
+app.get(`/res1/:idNum`, reservationController.getProfile1);
+app.get(`/res2/:idNum`, reservationController.getProfile2);
+app.get(`/res3/:idNum`, reservationController.getProfile3);
 
 app.get(`/logout`, controller.redirectRoot); // calling function to LOGOUT the website
 
