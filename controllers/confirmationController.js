e41b3c9f0034809b67f556ddda41965909156647
Jesca Ryanne Acosta <89693266+jescaryanneacosta@users.noreply.github.com>
2023-07-24 13:15:28 +0800

    Update confirmationController.js

--- controllers/confirmationController.js
+++ controllers/confirmationController.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const Handlebars = require('handlebars');
 
 // Import module `database` from `../models/db.js`
 const db = require('../models/db.js');
@@ -10,15 +9,16 @@ const User = require('../models/UserModel.js');
 // Fetch the reservation data based on the profileId
 async function fetchReservationData(profileId) {
     // Logic to fetch the reservation data from the database
-    
+    // ... Your code to fetch the reservation data ...
+    // Sample data for illustration purposes
     const reservationData = {
-        firstname: '',
-        lastname: '',
-        idNum: '',
-        roles: '',
-        roomNumber: '',
-        timeslot: '',
-        seatNumbers: ''
+        firstname: 'John',
+        lastname: 'Doe',
+        idNum: '123456',
+        roles: 'Guest',
+        roomNumber: 'COMPUTER LAB #1',
+        timeslot: '9:00 AM - 10:30 AM',
+        seatNumbers: 'A1, A2, A3'
     };
     return reservationData;
 }
@@ -30,21 +30,6 @@ async function renderConfirmationPage(req, res) {
     const reservationData = await fetchReservationData(profileId);
 
     if (reservationData) {
-        // Specify the Handlebars partial to use based on the reservation profile
-        let partialName = '';
-        if (profileId === 'res1' || profileId === 'res2' || profileId === 'res3') {
-            partialName = 'reservationDetails'; // Use the same partial for all profiles if they share the same structure
-        } else {
-            // Handle invalid profileId
-            return res.render('error');
-        }
-
-        // Read the partial file and register it with Handlebars (if not already registered)
-        if (!Handlebars.partials[partialName]) {
-            const partialContent = fs.readFileSync(`views/${partialName}.hbs`, 'utf8');
-            Handlebars.registerPartial(partialName, partialContent);
-        }
-
         // Render the confirmation.hbs file with the reservation data
         res.render('confirmation', reservationData);
     } else {
