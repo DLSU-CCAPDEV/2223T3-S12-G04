0f3392cc2c90d3cfbe5113b31081df9f546b4d62
Jesca Ryanne Acosta <89693266+jescaryanneacosta@users.noreply.github.com>
2023-07-24 13:21:15 +0800

    Update confirmationController.js

--- controllers/confirmationController.js
+++ controllers/confirmationController.js
@@ -6,21 +6,37 @@ const db = require('../models/db.js');
 // Import module `User` from `../models/UserModel.js`
 const User = require('../models/UserModel.js');
 
-// Fetch the reservation data based on the profileId
+// Function to fetch reservation data from the database
 async function fetchReservationData(profileId) {
-    // Logic to fetch the reservation data from the database
-    // ... Your code to fetch the reservation data ...
-    // Sample data for illustration purposes
-    const reservationData = {
-        firstname: 'John',
-        lastname: 'Doe',
-        idNum: '123456',
-        roles: 'Guest',
-        roomNumber: 'COMPUTER LAB #1',
-        timeslot: '9:00 AM - 10:30 AM',
-        seatNumbers: 'A1, A2, A3'
-    };
-    return reservationData;
+    try {
+        // Your actual database query to fetch reservation data from the database
+        const query = { idNum: profileId }; // Assuming `idNum` is the field used to identify the profile
+        const projection = 'firstname lastname idNum roles roomNumber timeslot seatNum'; // Include `seatNum` in projection
+        const result = await db.findOne(User, query, projection);
+
+        // Check if the user's reservation data was found in the database
+        if (result) {
+            // Extract reservation details from the result and return them
+            const reservationData = {
+                firstname: result.firstname,
+                lastname: result.lastname,
+                idNum: result.idNum,
+                roles: result.roles,
+                roomNumber: result.roomNumber,
+                timeslot: result.timeslot,
+                seatNumbers: result.seatNum // Use `seatNum` field for seat numbers
+            };
+
+            return reservationData;
+        } else {
+            // User's reservation data not found
+            return null;
+        }
+    } catch (err) {
+        // Handle any errors that occur during the database query
+        console.error('Error fetching reservation data:', err);
+        throw err; // You can choose to handle the error in a different way if needed
+    }
 }
 
 // Function to render the confirmation page
@@ -33,9 +49,10 @@ async function renderConfirmationPage(req, res) {
         // Render the confirmation.hbs file with the reservation data
         res.render('confirmation', reservationData);
     } else {
-        // Handle invalid reservation profile
+        // Handle invalid reservation profile or database errors
         res.render('error');
     }
 }
 
 module.exports = { renderConfirmationPage };
+
