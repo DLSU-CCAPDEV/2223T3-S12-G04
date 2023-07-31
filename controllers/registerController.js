4c8be45bb711bf6667640b4a96f663796fc18515
Alfonso Dionisio <134362662+Fondve@users.noreply.github.com>
2023-07-31 18:24:20 +0800

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,3 +1,10 @@
+// import module `validationResult` from `express-validator`
+const { validationResult } = require('express-validator');
+
+// import module `bcrypt`
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 // import module `database` from `../models/db.js`
 const db = require('../models/db.js');
 
@@ -11,7 +18,20 @@ const registerController = {
 	},
 	
 	postRegister: async function (req, res) {
+		
+		//Checker for register errors
+		
+		var errors = validationResult(req);
 
+		if (!errors.isEmpty()) {
+        errors = errors.errors;
+
+        var details = {};
+        for(i = 0; i < errors.length; i++)
+            details[errors[i].param + 'Error'] = errors[i].msg;
+
+        res.render('register', details);
+    }
         /*
             when submitting forms using HTTP POST method
             the values in the input fields are stored in `req.body` object
@@ -19,31 +39,55 @@ const registerController = {
             Example: the value entered in <input type="text" name="fName">
             can be retrieved using `req.body.fName`
         */
+		
+		else {
+			
         var roles = req.body.roles;
         var firstname = req.body.firstname;
         var lastname = req.body.lastname;
         var email = req.body.email;
 		var idNum = req.body.idNum;
         var pw = req.body.pw;
-
+		
+		bcrypt.hash(pw, saltRounds, async function(err, hash) {
+			
         var user = {
         	roles: roles,
             firstname: firstname,
             lastname: lastname,
             email: email,
 			idNum: idNum,
-            pw: pw
+            pw: hash
         }
-        var response = await db.insertOne(User, user);
-
-        if(response != null){
-            res.redirect('/success?roles=' + roles + '&firstname=' + firstname +'&lastname=' + lastname + '&idNum=' + idNum);
+        var flag = await db.insertOne(User, user);
+		
+                    if(flag) { 
+                        res.redirect('/success?roles=' + roles + '&firstname=' + firstname +'&lastname=' + lastname + '&idNum=' + idNum);
+                    }
+            });
         }
-        else {
-            res.render('error');
-        }
-    }
+    },
 	
+	getCheckID: async function (req, res) {
+
+        /*
+            when passing values using HTTP GET method
+            the values are stored in `req.query` object
+            Example url: `http://localhost/getCheckID?idNum=11312345`
+            To retrieve the value of parameter `idNum`: `req.query.idNum`
+        */
+        var idNum = req.query.idNum;
+
+        /*
+            calls the function findOne()
+            defined in the `database` object in `../models/db.js`
+            searches for a single document based on the model `User`
+            sends an empty string to the user if there are no match
+            otherwise, sends an object containing the `idNum`
+        */
+        var result = await db.findOne(User, {idNum: idNum}, 'idNum');
+        res.send(result);
+    }
 }
 
 module.exports = registerController;
